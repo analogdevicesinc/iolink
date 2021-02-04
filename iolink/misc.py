0f39ea3a2f915e558961cd8bf007cad5f4c9a1ea
bp <benjamin.partzsch@maximintegrated.com>
2021-02-04 08:59:00 +0100

    fix: iqlink to iqLink

--- iolink/misc.py
+++ iolink/misc.py
@@ -1,17 +1,15 @@
-
 from .interfaces.iqlink.iqlink import IqLinkPort
 
 from contextlib import contextmanager
 
-
-available_interfaces = {'iqlink': IqLinkPort}
+available_interfaces = {'iqLink': IqLinkPort}
 
 
 @contextmanager
 def get_port(interface):
     """Factory of specific instances of the abstract Port class.
 
-    :param str interface: ID of your IO-Link master device - currently only `iqlink` is supported.
+    :param str interface: ID of your IO-Link master device - currently only `iqLink` is supported.
     """
     port = available_interfaces[interface]()
     yield port
